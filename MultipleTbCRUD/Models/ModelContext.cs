1500b5b2f2a90f056ffda62ff0fcb3e01bdb586b
Eel2000 <eliezerbwana@gmaill.com>
2020-07-11 13:24:43 +0200

    Adding Dbcontext

---
+++ MultipleTbCRUD/Models/ModelContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleTbCRUD.Models
+{
+    public class ModelContext : DbContext
+    {
+
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
