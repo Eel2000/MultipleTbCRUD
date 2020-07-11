a389e55671e40dc5b727161ba0dcde376cf0ece4
Eel2000 <eliezerbwana@gmaill.com>
2020-07-11 13:44:20 +0200

    adding class to create automaticaly dropdown list and manage it

---
+++ MultipleTbCRUD/Pages/Library/CategoryNamePageModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MultipleTbCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleTbCRUD.Pages.Library
+{
+    public class CategoryNamePageModel : PageModel
+    {
+        public SelectList CategoryNameSL { get; set; }
+
+        public void PopulateCategoryDropDownList(ModelContext _context, object selectedCategory = null)
+        {
+            var CategoryQuery = from c in _context.Categories
+                                orderby c.Designation //sort by designation
+                                select c;
+
+            CategoryNameSL = new SelectList(CategoryQuery.AsNoTracking(), "CategoryID", "Designation", selectedCategory);
+        }
+    }
+}
